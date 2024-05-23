0f7f70a7f32744b7234d93bef864e61f9111ce65
Your Name <your_email@example.com>
2024-05-23 16:15:05 +0300

    er

--- app/ui/dashboard/CandidateDetails/CandidateDetails.tsx
+++ app/ui/dashboard/CandidateDetails/CandidateDetails.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 
 export default function CandidateDetails({ candidate }) {
   const renderProfessions = (professions) => {
@@ -5,50 +6,50 @@ export default function CandidateDetails({ candidate }) {
       return "нет профессий";
     }
     return professions.map((prof, index) => (
-      <p key={index} className='flex flex-col'>
-        <p>{prof.name}</p>
-        <small>{prof.experience}</small>
-      </p>
+      <div key={index} className='flex '>
+        <p>{prof.name}:</p> <span className="badge badge-success">опыт {prof.experience}</span>
+      </div>
     ));
   };
+
   const renderDocuments = (documents) => {
     if (!documents || documents.length === 0) {
       return "нет документов";
     }
     return documents.map((doc, index) => (
-      <p key={index} className='flex flex-col'>
-        <p>{doc.docType}</p>
-        <p>{doc.dateExp}</p>
-      </p>
+      <div key={index} className='flex '>
+        <p>{doc.docType}: <span className="badge badge-neutral">{doc.dateExp ? doc.dateExp: "дата не указана"}</span></p>
+        
+      </div>
     ));
   };
- 
 
+  console.log('Candidate Data:', candidate); // Добавим отладочное сообщение
 
- 
-    return (
-        <>
-        <div className="">
-            <h1 className="font-bold py-10 text-2xl">Update Candidate</h1>
-        </div>
-        <div >
-        <div >
-          <div className='flex flex-col w-full'>
-          
-<div >Имя: {candidate.name}</div>
-<div>Возраст: {candidate.age}</div>
-<div >Номер телефона: {candidate.phone}</div>
-<div>Местоположение: {candidate.locations}</div>
-<div>Знание языка: {candidate.langue.name}</div>
-<div>Статус: {candidate.status}</div>
-<div>Статус трудоустройства: <span>{candidate.statusFromPartner.status}</span>работает <span>{candidate.statusFromPartner.who}</span></div>
-<div>Менеджер: {candidate.manager.name}</div>
-<div>Номер счёта: {candidate.cardNumber}</div>
-<div>Профессии: {renderProfessions(candidate.professions)}</div>
-<div>Документы: {renderDocuments(candidate.documents)}</div>
+  return (
+    <>
+      <div className="flex">
+        <h1 className="font-bold py-10 text-2xl text-center">Анкета {candidate.name}  </h1>
+        <h2 className='flex gap-3'>Профессии: {renderProfessions(candidate.professions)}</h2>
+      </div>
+      <div>
+        <div>
+          <div className='flex flex-col w-full gap-2'>
+            <div>Имя: {candidate.name}</div>
+            <div>Возраст: {candidate.age}</div>
+            <div>Номер телефона: {candidate.phone}</div>
+            <div>Местоположение: {candidate.locations}</div>
+            <div>Знание языка: {candidate.langue?.name}</div>
+            <div>Статус: {candidate.status}</div>
+            <div>
+              Статус трудоустройства: <div><span className="badge badge-error">{candidate.statusFromPartner?.who}</span> <span className="badge badge-error">{candidate.statusFromPartner?.status}</span></div> 
+            </div>
+            <div>Менеджер: {candidate.manager?.name}</div>
+            <div>Номер счёта: {candidate.cardNumber ? candidate.cardNumber : "нет номера счёта"}</div>
+            <div>Документы: {renderDocuments(candidate.documents)}</div>
           </div>
-        </div>          
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
