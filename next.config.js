1ab6556f6bbde86a0eeab69b063de6ef68bdd909
Your Name <your_email@example.com>
2024-07-02 16:53:08 +0300

    tt

--- next.config.js
+++ next.config.js
@@ -1,3 +1,36 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "images.pexels.com",
+//       },
+//     ],
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'lh3.googleusercontent.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'avatars.githubusercontent.com',
+//       },
+//     ],
+//   },
+//   async middleware() {
+//     return [
+//       {
+//         source: "/((?!api|_next|static|public|favicon.ico).*)",
+//         destination: "/api/auth/middleware",
+//       },
+//     ];
+//   },
+// };
+
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -6,10 +39,6 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.pexels.com",
       },
-    ],
-  },
-  images: {
-    remotePatterns: [
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
@@ -19,6 +48,9 @@ const nextConfig = {
         hostname: 'avatars.githubusercontent.com',
       },
     ],
+  },
+  experimental: {
+    disableMiddlewareWarnings: true,
   },
   async middleware() {
     return [
