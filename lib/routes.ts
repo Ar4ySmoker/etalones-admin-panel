842c79e6ca764059d486b82b3843b7d5c920a42c
Your Name <your_email@example.com>
2024-09-20 14:26:23 +0300

    11

--- lib/routes.ts
+++ lib/routes.ts
@@ -2,5 +2,5 @@
 
 export const ROOT = "/account";
 export const PUBLIC_ROUTES = [ROOT];
-export const PROTECTED_ROUTES = ["/dashboard","/dashboard/candidates", "/dashboard/:path*"]; // Добавьте другие защищённые маршруты
+export const PROTECTED_ROUTES = ["/dashboard", "/dashboard/:path*"]; // Добавьте другие защищённые маршруты
 export const DEFAULT_REDIRECT = ROOT; // Или другую страницу
